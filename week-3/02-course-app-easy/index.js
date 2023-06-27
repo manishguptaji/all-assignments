a79e81655b8c1e40251f45107d9c6badf8078013
Manish Gupta <guptamanish181992@gmail.com>
2023-06-27 10:52:03 -0700

    1 commit

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,13 +7,40 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+async function logInAdmin(req, res) {
+  // logic to log in admin
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const index = await ADMINS.findIndex(
+    (admin) => admin.username === username && admin.password === password
+  );
+  return index;
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const { username } = req.body;
+
+  //check if admin with same creds alreday exists
+  const index = ADMINS.findIndex((admin) => admin.username === username);
+  if (index > -1) {
+    res.status(400).send("Admin already Exists");
+  }
+  ADMINS.push(req.body);
+  res.status(200).json({ message: "Admin created successfully" });
 });
 
 app.post('/admin/login', (req, res) => {
   // logic to log in admin
+  logInAdmin(req, res).then((index) => {
+    if (index <= -1) {
+      res.status(400).send("No Admin with such creds exists");
+    } else {
+      isUserLoggedIn = true;
+      res.status(201).json({ message: "Logged in successfully" });
+    }
+  });
 });
 
 app.post('/admin/courses', (req, res) => {
