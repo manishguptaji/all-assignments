bcc5de571f56fddd694ceab4f20ac64241bdc72d
Manish Gupta <guptamanish181992@gmail.com>
2023-07-01 11:17:43 -0700

    added middle ware

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,18 +7,16 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-function isValidAdmin(headers) {
-  const username = headers.username;
-  const password = headers.password;
-  let userFound = false;
 
-  ADMINS.forEach(admin => {
-    if(admin.userName === username && admin.password === password) {
-      userFound = true;  
-    }
-  });
+const adminMiddleWare = (req, res, next) => {
+  const {username, password} = req.headers;
 
-  return userFound;
+  const isAdmin = ADMINS.find((admin) => admin.userName === username && admin.password === password)
+  if(isAdmin) {
+    next();
+  } else {
+    res.status(403).send("No Admin with such creds exists");
+  }
 }
 
 function isValidCourse(courseId) {
@@ -33,8 +31,8 @@ app.post('/admin/signup', (req, res) => {
   const { userName } = req.body;
 
   //check if admin with same creds alreday exists
-  const index = ADMINS.findIndex((admin) => admin.userName === userName);
-  if (index > -1) {
+  const index = ADMINS.find((admin) => admin.userName === userName);
+  if (index) {
     res.status(400).send("Admin already Exists");
   } else {
     ADMINS.push(req.body);
@@ -42,55 +40,34 @@ app.post('/admin/signup', (req, res) => {
   }
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
-  let userValid = isValidAdmin(req.headers);
-  if (!userValid) {
-    res.status(400).send("No Admin with such creds exists");
-  } else {  
-    res.status(201).json({ message: "Logged in successfully" });
-  }
+app.post('/admin/login', adminMiddleWare, (req, res) => {
+  res.status(200).json({ message: "Logged in successfully" });
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', adminMiddleWare, (req, res) => {
   // logic to create a course
   const body = req.body;
-  const headers = req.headers;
   const _id = COURSES.length + 1;
 
-  const index = ADMINS.findIndex((admin) => admin.userName === headers.username && admin.password === headers.password);
-
-  if(index <= -1) {
-    res.status(400).send("No Admin with such creds exists");
-  } else {
-    const dataObject = {
-      id: _id,
-      title: body.title,
-      description: body.description,
-      price: body.price,
-      imgLink: body.imgLink,
-      published: body.published
-    }
-  
-    COURSES.push(dataObject);
-    res.status(200).json({message: 'done', id: _id});
-    //res.status(200).json(COURSES);
-  }
-});
-
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
-  if(isValidAdmin(req.headers)) {
-    res.status(200).json(COURSES);
-  } else {
-    res.status(401).json({msg: "Invalid creds"});
+  const dataObject = {
+    id: _id,
+    title: body.title,
+    description: body.description,
+    price: body.price,
+    imgLink: body.imgLink,
+    published: body.published
   }
 
+  COURSES.push(dataObject);
+  res.status(200).json({message: 'done', id: _id});
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.get('/admin/courses', adminMiddleWare, (req, res) => {
+  res.status(200).json(COURSES);
+});
+
+app.put('/admin/courses/:courseId', adminMiddleWare, (req, res) => {
   // logic to edit a course
-  const adminRole = isValidAdmin(req.headers);
   const body = req.body;
 
   if(adminRole) {
